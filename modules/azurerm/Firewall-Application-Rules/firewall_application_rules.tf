85889c92301eca26da61a0bfc1e1ab4f4a3b52d6
SazniMohamed <ameersazni@gmail.com>
2023-11-16 18:40:37 +0530

    Add module for firewall application rule collection

---
+++ modules/azurerm/Firewall-Application-Rules/firewall_application_rules.tf
@@ -0,0 +1,29 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "azurerm_firewall_application_rule_collection" "application_rule_collection" {
+  name                = join("-", ["fwapprc", var.firewall_network_rule_collection_name])
+  azure_firewall_name = var.firewall_name
+  resource_group_name = var.resource_group_name
+  priority            = var.priority
+  action              = var.action
+
+  dynamic "rule" {
+    for_each = var.network_rules
+
+    content {
+      name             = rule.value.name
+      source_addresses = rule.value.source_addresses
+      target_fqdns     = rule.value.target_fqdns
+      protocols        = rule.value.protocol
+    }
+  }
+}
