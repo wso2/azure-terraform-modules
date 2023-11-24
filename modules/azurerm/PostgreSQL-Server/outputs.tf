8aa4bfd7c66264dd9434a7c3f00e996effb12583
SazniMohamed <ameersazni@gmail.com>
2023-11-24 12:21:58 +0530

    Add Module for PostgreSQL Server

---
+++ modules/azurerm/PostgreSQL-Server/outputs.tf
@@ -0,0 +1,20 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+output "postgresql_server_id" {
+  value      = azurerm_postgresql_flexible_server.postgresql_flexible_server.id
+  depends_on = [azurerm_postgresql_flexible_server.postgresql_flexible_server]
+}
+
+output "postgresql_server_fqdn" {
+  value      = azurerm_postgresql_flexible_server.postgresql_flexible_server.fqdn
+  depends_on = [azurerm_postgresql_flexible_server.postgresql_flexible_server]
+}
