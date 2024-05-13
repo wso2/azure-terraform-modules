39a27d255fe61361294103e5129c5968cbcd3847
SazniMohamed <ameersazni@gmail.com>
2024-05-13 09:43:00 +0530

    Update azurerm_monitor_diagnostic_categories by removing retention period as it is not supported by azure

--- modules/azurerm/Resource-Monitoring-Diagnostic-Setting/locals.tf
+++ modules/azurerm/Resource-Monitoring-Diagnostic-Setting/locals.tf
@@ -1,21 +1,28 @@
 # -------------------------------------------------------------------------------------
 #
-# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+# Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
 #
-# This software is the property of WSO2 LLC. and its suppliers, if any.
-# Dissemination of any information or reproduction of any material contained
-# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
-# You may not alter or remove any copyright or other notice from copies of this content.
+# WSO2 LLC. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
 #
 # --------------------------------------------------------------------------------------
 
 locals {
-  user_provided_logs_settings      = { for s in var.required_log_categories : s.category_name => s.retention_period }
-  user_provided_metrics_settings   = { for s in var.required_metric_categories : s.category_name => s.retention_period }
-  default_logs_retention_period    = var.all_log_types_enabled.enabled ? var.all_log_types_enabled.retention_period : 0
-  default_metrics_retention_period = var.all_metrics_enabled.enabled ? var.all_metrics_enabled.retention_period : 0
-  updated_default_logs_settings    = { for s in sort(data.azurerm_monitor_diagnostic_categories.default.logs) : s => local.default_logs_retention_period }
-  updated_default_metrics_settings = { for s in sort(data.azurerm_monitor_diagnostic_categories.default.metrics) : s => local.default_metrics_retention_period }
+  user_provided_logs_settings      = { for s in var.required_log_categories : s.category_name => true }
+  user_provided_metrics_settings   = { for s in var.required_metric_categories : s.category_name => true }
+  updated_default_logs_settings    = { for s in sort(data.azurerm_monitor_diagnostic_categories.default.logs) : s => var.all_log_types_enabled }
+  updated_default_metrics_settings = { for s in sort(data.azurerm_monitor_diagnostic_categories.default.metrics) : s => var.all_metrics_enabled }
   all_logs_settings                = merge(local.updated_default_logs_settings, local.user_provided_logs_settings)
   all_metrics_settings             = merge(local.updated_default_metrics_settings, local.user_provided_metrics_settings)
 }
