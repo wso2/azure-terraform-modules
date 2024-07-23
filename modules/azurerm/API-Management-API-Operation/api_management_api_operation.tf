85a64f1b04001739ebb2d492ac1d7286b6323448
Lakshan-Banneheke <btlakshan@gmail.com>
2024-07-23 15:47:08 +0530

    Add API Management API Operation

---
+++ modules/azurerm/API-Management-API-Operation/api_management_api_operation.tf
@@ -0,0 +1,31 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "azurerm_api_management_api_operation" "api_management_api_operation" {
+  operation_id        = join("-", [var.api_operation_abbreviation, var.api_operation_id])
+  api_name            = var.api_name
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+  display_name        = var.display_name
+  method              = var.http_method
+  url_template        = var.url_template
+
+  dynamic "template_parameter" {
+    for_each = var.template_parameters
+
+    content {
+      name     = template_parameter.value.name
+      type     = template_parameter.value.type
+      required = template_parameter.value.required
+      description = template_parameter.value.description
+    }
+  }
+}
---
+++ modules/azurerm/API-Management-API-Operation/outputs.tf
@@ -0,0 +1,30 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+output "id" {
+  value       = azurerm_api_management_api_operation.api_management_api_operation.id
+  depends_on  = [azurerm_api_management_api_operation.api_management_api_operation]
+}
+
+output "operation_id" {
+    value       = azurerm_api_management_api_operation.api_management_api_operation.operation_id
+    depends_on  = [azurerm_api_management_api_operation.api_management_api_operation]
+}
+
+output "api_name" {
+    value       = azurerm_api_management_api_operation.api_management_api_operation.api_name
+    depends_on  = [azurerm_api_management_api_operation.api_management_api_operation]
+}
+
+output "api_management_name" {
+    value       = azurerm_api_management_api_operation.api_management_api_operation.api_management_name
+    depends_on  = [azurerm_api_management_api_operation.api_management_api_operation]
+}
