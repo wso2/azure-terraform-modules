3e4973631d2c54e5394743d2ad2d1ab7739614e0
SazniMohamed <ameersazni@gmail.com>
2024-09-24 15:41:37 +0530

    Update subplane for azure security center subscription prociing

--- modules/azurerm/Azure-Security-Center-Setting/variables.tf
+++ modules/azurerm/Azure-Security-Center-Setting/variables.tf
@@ -23,6 +23,7 @@ variable "pricing_setting" {
 variable "subplan" {
   description = "Resource type pricing subplan. Contact your MSFT representative for possible values."
   type        = string
+  default     = ""
 }
 
 variable "extensions" {
