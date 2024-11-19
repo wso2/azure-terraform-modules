e7a04dd124a372ac72e79daa80cd46ca42881d26
Vimukthi Perera <vimmperera@gmail.com>
2024-11-19 13:35:36 +0530

    Add resource name prefix

--- modules/azurerm/Synapse-Workspace/synapse_workspace.tf
+++ modules/azurerm/Synapse-Workspace/synapse_workspace.tf
@@ -19,7 +19,7 @@
 # --------------------------------------------------------------------------------------
 
 resource "azurerm_synapse_workspace" "synapse_workspace" {
-  name                                 = var.name
+  name                                 = join("-", ["synw", var.name])
   resource_group_name                  = var.resource_group_name
   location                             = var.location
   storage_data_lake_gen2_filesystem_id = var.storage_data_lake_gen2_filesystem_id
