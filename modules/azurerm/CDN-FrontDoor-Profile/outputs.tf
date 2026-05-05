fb02436dd30cd0a94db246eccd65e38bb811d84f
Vishakh Shetty <vishakhmugwa1999@gmail.com>
2026-05-05 09:58:48 +0530

    Update cdn-frontdoor-profile to handle identity ids

--- modules/azurerm/CDN-FrontDoor-Profile/outputs.tf
+++ modules/azurerm/CDN-FrontDoor-Profile/outputs.tf
@@ -32,3 +32,8 @@ output "resource_guid" {
   depends_on = [azurerm_cdn_frontdoor_profile.cdn_frontdoor_profile]
   value      = azurerm_cdn_frontdoor_profile.cdn_frontdoor_profile.resource_guid
 }
+
+output "identity" {
+  depends_on = [azurerm_cdn_frontdoor_profile.cdn_frontdoor_profile]
+  value      = azurerm_cdn_frontdoor_profile.cdn_frontdoor_profile.identity
+}
